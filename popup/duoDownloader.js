06c3ae13cc80e1aca7df34f12c9edbebae51ff5b
Firelord777 <moritz@mhuckle.de>
2023-07-18 17:52:07 +0200

    Almost finished real download Button

--- popup/duoDownloader.js
+++ popup/duoDownloader.js
@@ -1,4 +1,7 @@
+var global = window;
+
 let currentTab = "";
+let rawJson = "";
 
 // Check to test if the user currently is on Duolingo
 const divIsDuo = document.getElementById("isDuo");
@@ -12,8 +15,6 @@ browser.tabs.query({ active: true, currentWindow: true })
 function compareTab(tabs){
     currentTab = tabs[0]; // Get the current tabs
     const url = currentTab.url;
-
-    console.log(tabs[0].id + "comp");
     
     const isDuo = url.includes("www.duolingo.com/");
     
@@ -29,20 +30,53 @@ function onError(error) {
 
 // A button to fetch and download the raw Json vocabList from Duolingo.
 // Goes through a content script to circumvent CORS restrictions.
-const buttonDownloadRawJson = document.getElementById("downloadRawJson");
+const buttonLoadVocab = document.getElementById("loadVocab");
 
+buttonLoadVocab.addEventListener("click", loadVocab);
+
+function loadVocab(){
+  browser.tabs.sendMessage(currentTab.id, "getVocabList").then(vocabDownloaded, onError); //T Add proper Error Handling.
+}
+
+const divDownloadComplete = document.getElementById("downloadComplete");
+
+async function vocabDownloaded(response){
+  rawJson = response;
+
+  divIsDuo.hidden = true;
+  divDownloadComplete.hidden = false;
+
+  const courseLanguage = document.getElementById("courseLanguage");
+
+  courseLanguage.innerHTML = rawJson.language_string;
+}
+
+const buttonDownloadRawJson = document.getElementById("downloadRawJson");
 buttonDownloadRawJson.addEventListener("click", downloadRawJson);
 
-function downloadRawJson(){
-  browser.tabs.sendMessage(currentTab.id, "getVocabList").then(rawJsonDownloaded, onError); //T Add proper Error Handling.
+
+function downloadRawJson() {
+  downloadFile(JSON.stringify(rawJson))
 }
 
-async function rawJsonDownloaded(response){
-  
-  //T Do something with the response.
-  // Or put the whole response Handeling into a background script.
-  console.log(response);
-}
+function downloadFile(text){
+  // Create a new Blob object from the data
+  const blob = new Blob([text], { type: 'application/json' });
 
+  // Generate a temporary URL for the blob
+  const url = URL.createObjectURL(blob);
+
+
+  // Create an anchor element to trigger the file download
+  const downloadLink = document.createElement('a');
+  downloadLink.href = url;
+  downloadLink.download = 'data.json'; // Specify the filename and extension for the saved file
+  downloadLink.click();
+
+  // Clean up the temporary URL after the download is initiated
+  URL.revokeObjectURL(url);
+
+  downloadLink.remove();
+}
 
 
